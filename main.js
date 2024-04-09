47698dbe1cf37eafa06ceee20b709bac4d4f41e6
ingridrafaela <105295591+ingridrafaela@users.noreply.github.com>
2024-04-09 11:06:41 -0300

    .

---
+++ main.js
@@ -0,0 +1,11 @@
+const botoes = document.querySelectorA11(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botao[i].onclick = function() {
+
+        for(let j=0;)
+
+        botoes{i}.classList.add("ativo");
+    }
+    console.log();
+}
